04f072509e825b2bd1db86aeb87ebb789df12df3
shk <zezic51@yandex.ru>
2018-07-30 15:20:53 +0300

    Handle 404.

--- components/Comments.vue
+++ components/Comments.vue
@@ -10,12 +10,10 @@
 
 <script>
 import Vue from 'vue'
+import striptags from 'striptags'
 
 Vue.filter('striphtml', function (value) {
-  var div = document.createElement("div")
-  div.innerHTML = value
-  var text = div.textContent || div.innerText || ""
-  return text
+  return striptags(value)
 })
 
 export default {
--- pages/_slug.vue
+++ pages/_slug.vue
@@ -4,10 +4,38 @@
 </template>
 
 <script>
+import striptags from 'striptags'
+import { mapGetters, mapState } from 'vuex'
 import Album from '~/components/Album.vue'
 
 export default {
   name: 'album-page',
+  asyncData ({ error, params, store }) {
+    const slug = params.slug
+    // console.log(store.getters)
+    if (!slug) {
+      return
+    }
+    if (!store.getters.albums.find(album => album.slug === slug)) {
+      error({ statusCode: 404, message: 'Album not found'})
+    }
+  },
+  head () {
+    return {
+      title: `${this.album.title} by ${this.author.title}`,
+      meta: [
+        { hid: 'description', name: 'description', content: striptags(this.album.content) }
+      ]
+    }
+  },
+  computed: {
+    ...mapGetters(['albums']),
+    ...mapState(['author']),
+    album () {
+      const slug = this.$route.params.slug
+      return slug ? this.albums.find(album => album.slug === slug) : this.albums[0]
+    }
+  },
   components: {
     Album
   }
